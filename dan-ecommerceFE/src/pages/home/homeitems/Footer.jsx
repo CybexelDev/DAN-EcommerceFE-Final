8557268a19684f5cf9c8b76eb1baa196fd7b73bf
N-meapp <developersnme@gmail.com>
2026-01-19 17:31:12 +0530

    footer update

--- dan-ecommerceFE/src/pages/home/homeitems/Footer.jsx
+++ dan-ecommerceFE/src/pages/home/homeitems/Footer.jsx
@@ -13,7 +13,7 @@ function Footer() {
                     after:-bottom-[1.5vw] after:left-[0vw]
           ">
                 <div className="w-[84%] aspect-[215/36]  ms-[9.5%] ">
-                    <p className='text-[1.45vw]'>Dan Dar Al Nahda Tr.</p></div>
+                    <p className='text-[1.20vw]'>Dan Dar Al Nahda Tr.</p></div>
             </div>
             {/* Top button Section end */}
 
@@ -25,14 +25,14 @@ function Footer() {
                 <div className=" w-[100%]   h-[90%]  flex flex-col md:flex-row justify-between">
                     <div className="w-[100%] md:w-[42%] lg:w-[33%] h-full lg:h-[90%] lg:aspect-[2/1] py-[5vw] md:p-0 flex flex-col justify-between gap-[5vw] md:gap-[2vw] lg:gap-0 ">
                         <div className="h-[32.5%] w-[100%]    ">
-                            <h4 className=' text-[4.5vw] md:text-[2.3vw] lg:text-[1.85vw] font-semibold'>Join our newsletter and get 20%
+                            <h4 className=' text-[4.5vw] md:text-[2.3vw] lg:text-[1.40vw] font-semibold'>Join our newsletter and get 20%
                                 off your first purchase with us.</h4>
                         </div>
 
                         <div className="h-[32%] w-[100%]  bg-white rounded-[2.5vw] md:rounded-[1vw]    ">
-                            <form action="" className='flex justify-between items-center  w-[100%] h-[95%] shadow   '>
+                            <form action="" className='flex justify-between items-center  w-[100%] h-[100%]  '>
                                 <input type="email" placeholder='Your Email Address'
-                                    className='text-[4vw]   md:text-[1.3vw]  w-[85%] md:w-[74.5%] h-full p-[3%] flex items-center ' />
+                                    className='text-[4vw]   md:text-[1.1vw]  w-[85%] md:w-[74.5%] h-full p-[3%] flex items-center ' />
                                 <button className='text-[4.5vw]   md:text-[1.3vw] text-white w-[25.5%] h-[100%] lg:h-[94%] p-[1.5%] md:p-[2.5%] rounded-[2vw] md:rounded-[1vw] flex items-center justify-center  mx-[.5vw] md:mx-[.2vw] bg-[#8345D8]'>Join</button>
                             </form>
                         </div>
@@ -47,11 +47,11 @@ function Footer() {
                                     <h6 className='font-semibold'>Pages</h6>
                                 </div>
                                 <div className="h-[76.75%]  flex flex-col justify-between  ">
-                                    <div className="text-[clamp(1rem,1.1rem,1.3rem)] md:text-[clamp(.9rem,1.1rem,1.3rem)] lg:text-[clamp(.7rem,.9rem,1.1rem)] xl:text-[clamp(.9rem,1.1rem,1.3rem)] font-semibold text-black/55 cursor-pointer" onClick={() => navigate('/')}>Home</div>
-                                    <div className="text-[clamp(1rem,1.1rem,1.3rem)] md:text-[clamp(.9rem,1.1rem,1.3rem)] lg:text-[clamp(.7rem,.9rem,1.1rem)] xl:text-[clamp(.9rem,1.1rem,1.3rem)] font-semibold text-black/55 cursor-pointer" onClick={() => navigate('/collections')}>Collections</div>
-                                    <div className="text-[clamp(1rem,1.1rem,1.3rem)] md:text-[clamp(.9rem,1.1rem,1.3rem)] lg:text-[clamp(.7rem,.9rem,1.1rem)] xl:text-[clamp(.9rem,1.1rem,1.3rem)] font-semibold text-black/55 cursor-pointer" onClick={() => navigate('/cart')}>Cart</div>
-                                    <div className="text-[clamp(1rem,1.1rem,1.3rem)] md:text-[clamp(.9rem,1.1rem,1.3rem)] lg:text-[clamp(.7rem,.9rem,1.1rem)] xl:text-[clamp(.9rem,1.1rem,1.3rem)] font-semibold text-black/55 cursor-pointer" onClick={() => navigate('/blog')} >Blog</div>
-                                    <div className="text-[clamp(1rem,1.1rem,1.3rem)] md:text-[clamp(.9rem,1.1rem,1.3rem)] lg:text-[clamp(.7rem,.9rem,1.1rem)] xl:text-[clamp(.9rem,1.1rem,1.3rem)] font-semibold text-black/55 cursor-pointer" onClick={() => navigate('/orders')} >Orders</div>
+                                    <div className="text-[clamp(1rem,1.1rem,1.3rem)] md:text-[clamp(.9rem,1.1rem,1.3rem)] lg:text-[clamp(.7rem,.9rem,1.1rem)] xl:text-[16px] text-black/55 cursor-pointer" onClick={() => navigate('/')}>Home</div>
+                                    <div className="text-[clamp(1rem,1.1rem,1.3rem)] md:text-[clamp(.9rem,1.1rem,1.3rem)] lg:text-[clamp(.7rem,.9rem,1.1rem)] xl:text-[16px] text-black/55 cursor-pointer" onClick={() => navigate('/collections')}>Collections</div>
+                                    <div className="text-[clamp(1rem,1.1rem,1.3rem)] md:text-[clamp(.9rem,1.1rem,1.3rem)] lg:text-[clamp(.7rem,.9rem,1.1rem)] xl:text-[16px] text-black/55 cursor-pointer" onClick={() => navigate('/cart')}>Cart</div>
+                                    <div className="text-[clamp(1rem,1.1rem,1.3rem)] md:text-[clamp(.9rem,1.1rem,1.3rem)] lg:text-[clamp(.7rem,.9rem,1.1rem)] xl:text-[16px] text-black/55 cursor-pointer" onClick={() => navigate('/blog')} >Blog</div>
+                                    <div className="text-[clamp(1rem,1.1rem,1.3rem)] md:text-[clamp(.9rem,1.1rem,1.3rem)] lg:text-[clamp(.7rem,.9rem,1.1rem)] xl:text-[16px] text-black/55 cursor-pointer" onClick={() => navigate('/orders')} >Orders</div>
                                 </div>
                             </div>
                             <div className="w-[50%] md:w-[55%] flex flex-col ">
@@ -59,17 +59,17 @@ function Footer() {
                                     <h6 className=' font-semibold'>Information</h6>
                                 </div>
                                 <div className="h-[76.75%]  flex flex-col justify-between">
-                                    <div className="text-[clamp(1rem,1.1rem,1.3rem)] md:text-[clamp(.7rem,.9rem,1.1rem)] xl:text-[clamp(.9rem,1.1rem,1.3rem)] font-semibold text-black/55 cursor-pointer" onClick={() => navigate('/terms-and-conditions')}>Terms&Conditions</div>
-                                    <div className="text-[clamp(1rem,1.1rem,1.3rem)] md:text-[clamp(.7rem,.9rem,1.1rem)] xl:text-[clamp(.9rem,1.1rem,1.3rem)] font-semibold text-black/55 cursor-pointer" onClick={() => navigate('/privacy-policy')}>Privacy policy</div>
-                                    <div className="text-[clamp(1rem,1.1rem,1.3rem)] md:text-[clamp(.7rem,.9rem,1.1rem)] xl:text-[clamp(.9rem,1.1rem,1.3rem)] font-semibold text-black/55 cursor-pointer" onClick={() => navigate('/support')}>Support</div>
-                                    <div className="text-[clamp(1rem,1.1rem,1.3rem)] md:text-[clamp(.7rem,.9rem,1.1rem)] xl:text-[clamp(.9rem,1.1rem,1.3rem)] font-semibold  text-black/55 cursor-pointer" >404</div>
+                                    <div className="text-[clamp(1rem,1.1rem,1.3rem)] md:text-[clamp(.7rem,.9rem,1.1rem)] xl:text-[16px] text-black/55 cursor-pointer" onClick={() => navigate('/terms-and-conditions')}>Terms&Conditions</div>
+                                    <div className="text-[clamp(1rem,1.1rem,1.3rem)] md:text-[clamp(.7rem,.9rem,1.1rem)] xl:text-[16px] text-black/55 cursor-pointer" onClick={() => navigate('/privacy-policy')}>Privacy policy</div>
+                                    <div className="text-[clamp(1rem,1.1rem,1.3rem)] md:text-[clamp(.7rem,.9rem,1.1rem)] xl:text-[16px] text-black/55 cursor-pointer" onClick={() => navigate('/support')}>Support</div>
+                                    <div className="text-[clamp(1rem,1.1rem,1.3rem)] md:text-[clamp(.7rem,.9rem,1.1rem)] xl:text-[16px] text-black/55 cursor-pointer" >404</div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
                 <div className="w-[100%] h-full lg:h-[10%] flex lg:justify-start justify-center mt-[4vw] md:mt-[2vw] md:my-[1vw]  items-center ">
-                    <h6 className='text-[3.5vw] font-semibold md:text-[1.5vw] text-black/65'>Copyright <span className='font-semibold text-black' onClick={() => navigate('/admin')}>Daralnahdatrading</span> <span className=''>©</span> 2024</h6>
+                    <h6 className='text-[3.5vw] font-semibold md:text-[1.1vw] text-black/65'>Copyright <span className='font-semibold text-black' onClick={() => navigate('/admin')}>Daralnahdatrading</span> <span className=''>©</span> 2024</h6>
                 </div>
             </div>
             {/* Main Section End */}
